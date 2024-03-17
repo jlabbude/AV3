a864ad93a35b5dd3752c4ba8fe20bd3cf87afb40
BuildTools <unconfigured@null.spigotmc.org>
2024-03-17 13:54:18 -0300

    finalizando geração de mapa de calor

--- src/main/java/com/unijorge/devsoft/AV3/Controller.java
+++ src/main/java/com/unijorge/devsoft/AV3/Controller.java
@@ -2,8 +2,13 @@ package com.unijorge.devsoft.AV3;
 
 import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
 import com.github.prominence.openweathermap.api.model.Coordinate;
-import com.github.prominence.openweathermap.api.model.air.pollution.AirPollutionDetails;
-import com.github.prominence.openweathermap.api.model.air.pollution.AirPollutionRecord;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,41 +20,82 @@ import java.util.List;
 
 @RestController
 public class Controller {
-    static final Coordinate salvadorStart = Coordinate.of(-12.798580, -38.525089);
-    static final Coordinate salvadorEnd = Coordinate.of(-13.017439, -38.216442);
+    static final Coordinate salvadorEnd = Coordinate.of(-13.017222, -38.534444);
+    static final Coordinate salvadorStart = Coordinate.of(-12.784722, -38.185202);
     private static final double diff = 0.01;
 
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+
+    //TODO: analisar possibilidade de diminuir diff
+
+    //como cada vez que diminuirmos o tamanho de cada pixel
+    //no mapa, aumentamos a quantidade de requisições
+    //precisa-se analisar se é possível e/ou viável criar e guardar as keys adicionais
+
     @GetMapping("/pollution")
-    public List<AirPollutionRecord> collectData() {
+    public ResponseEntity<Resource> collectData(NoiseMapGenerator noiseMapGenerator) {
 
-        List<AirPollutionRecord> records = new ArrayList<>();
+        int keyIndex = 0;
+        int rateLimiter = 0;
+        List<String> keys = getKeys();
 
-        NoiseMapGenerator noiseMapGenerator = new NoiseMapGenerator();
+        for (int latitudeImage = 0; salvadorStart.getLatitude() > salvadorEnd.getLatitude(); salvadorStart.setLatitude(salvadorStart.getLatitude() - diff), latitudeImage++) {
 
-        for (; salvadorStart.getLatitude() > salvadorEnd.getLatitude(); salvadorStart.setLatitude(salvadorStart.getLatitude() - diff)) {
+            salvadorStart.setLongitude(-38.185202);
 
-            for (; salvadorStart.getLongitude() < salvadorEnd.getLongitude(); salvadorStart.setLongitude(salvadorStart.getLongitude() + diff)) {
+            for (int longitudeImage = 0; salvadorStart.getLongitude() > salvadorEnd.getLongitude(); salvadorStart.setLongitude(salvadorStart.getLongitude() - diff), longitudeImage++) {
 
-                AirPollutionDetails airPollutionDetails = new OpenWeatherMapClient("e2ce3793c593a0d749bd61edc88077c2")
+                String json = new OpenWeatherMapClient(keys.get(keyIndex))
                     .airPollution()
                     .current()
                     .byCoordinate(Coordinate.of(salvadorStart.getLatitude(), salvadorStart.getLongitude()))
                     .retrieve()
-                    .asJava();
+                    .asJSON();
 
-                noiseMapGenerator.generateNoiseMap(airPollutionDetails);
+                rateLimiter++;
 
-                records.addAll(airPollutionDetails.getAirPollutionRecords());
+                if(rateLimiter == 60) { keyIndex++; rateLimiter = 0; }
 
+                //String json = "[{\"coordinate\":{\"latitude\":40.71,\"longitude\":-74.01},\"airPollutionRecords\":[{\"forecastTime\":\"2024-03-14T17:16:37\", \"airQualityIndex\":\"FAIR\", \"o3\":94.41, \"co\":440.6, \"no\":8.05, \"no2\":37.01, \"so2\":8.94, \"pm2_5\":21.59, \"pm10\":27.09, \"nh3\":3.86, \"carbonMonoxide\":440.6, \"sulphurDioxide\":8.94, \"nitrogenDioxide\":37.01, \"coarseParticulateMatter\":27.09, \"fineParticlesMatter\":21.59, \"nitrogenMonoxide\":8.05, \"ozone\":94.41, \"ammonia\":3.86}]}]";
+                //String json = "{\"coord\":{\"lon\":-38.1852,\"lat\":-12.7847},\"list\":[{\"main\":{\"aqi\":1},\"components\":{\"co\":297.07,\"no\":0.08,\"no2\":0.35,\"o3\":37.19,\"so2\":0.77,\"pm2_5\":1.03,\"pm10\":4.8,\"nh3\":0.05},\"dt\":1710688227}]}";
+
+                logger.debug("json: {}", json);
+
+                try { noiseMapGenerator.noiseMapMapper(json, longitudeImage, latitudeImage); }
+                catch (ParseException e) { throw new RuntimeException(e); }
             }
         }
 
-        try {
-            ImageIO.write(, "png", new File("noise_map.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        try { ImageIO.write(NoiseMapGenerator.setTransparency(noiseMapGenerator.getImage()), "png", new File("noise_map.png")); }
+        catch (IOException e) { e.printStackTrace(); }
 
-        return records;
+
+        org.springframework.core.io.Resource resource = new FileSystemResource("noise_map.png");
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(resource);
+
+    }
+
+    private List<String> getKeys() {
+        List<String> keys = new ArrayList<>();
+
+        keys.add("e2ce3793c593a0d749bd61edc88077c2");
+        keys.add("1c49c3f1d2afe3d8d4d88be2a43a1170");
+        keys.add("50eafd1b3135ffb830f6df2d167ec5e7");
+        keys.add("d0ed64c75b924f60bc876d734802952d");
+        keys.add("8b932507812110b94ed5486256f3aafd");
+        keys.add("2643d51a664f8a58f3972ba999141bad");
+        keys.add("f2f4071bfc355f0cf0f231ada1f71a92");
+        keys.add("a7557c887a036c809d0801d68d057429");
+        keys.add("c7a5a9d64258e4f338c7df1f8d4f7d40");
+        keys.add("e5b4a83b87f78bc831c90eb212a88f0e");
+        keys.add("9ceec3ca92b46ffaca60f45c35096921");
+        keys.add("82293113285318a9878190a9f0999464");
+        keys.add("ff4b36362a6d48c8b8db774d3421083a");
+        keys.add("a1bc0ccc1ae70ed51af37f0fd3ad7fcf");
+        keys.add("e72bd190fdd8d900747fd6c27757f524");
+        keys.add("183f84dd6edb2738031fd2a84508774e");
+
+        return keys;
     }
 }
--- src/main/java/com/unijorge/devsoft/AV3/NoiseMapGenerator.java
+++ src/main/java/com/unijorge/devsoft/AV3/NoiseMapGenerator.java
@@ -1,44 +1,85 @@
 package com.unijorge.devsoft.AV3;
 
-import com.github.prominence.openweathermap.api.enums.AirQualityIndex;
-import com.github.prominence.openweathermap.api.model.air.pollution.AirPollutionDetails;
-import com.github.prominence.openweathermap.api.model.air.pollution.AirPollutionRecord;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
 
-import java.awt.Color;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import javax.imageio.ImageIO;
 
-import static com.unijorge.devsoft.AV3.Controller.salvadorEnd;
-import static com.unijorge.devsoft.AV3.Controller.salvadorStart;
-
+@Service
 public class NoiseMapGenerator {
 
-        public static final int IMAGE_WIDTH = 31;
-        public static final int IMAGE_HEIGHT = 22;
+        /**
+         * 805 queries (1 por pixel),
+         * 60 queries por minuto,
+         * 14 keys necessárias para processar
+         * o mapa sem esperar o rate limit acabar.
+         * Atualmente 1 pixel = ~ 1km²
+         * (aproximadamente a área de um bairro pequeno/médio)
+         */
+    public static final int IMAGE_WIDTH = 35;
 
-        public BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
+    public static final int IMAGE_HEIGHT = 24;
 
-    public void generateNoiseMap(AirPollutionDetails airPollutionDetails) {
+    public BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
 
-        double longitude = salvadorStart.getLongitude();
-        double latitude = salvadorEnd.getLatitude();
+    public void noiseMapMapper(String json, int longitude, int latitude) throws ParseException {
 
-        Color color = mapValueToColor();
+        Color color = mapValueToColor(getPollutionIndex(json));
 
-        //todo turn double into int
         getImage().setRGB(longitude, latitude, color.getRGB());
+
     }
 
     private Color mapValueToColor(int value) {
-        int gray = (255 * value / 4);
-        return new Color(gray, gray, gray);
+
+        int cor = (value * (255/5));
+
+        return new Color(cor, 0, 0);
+
     }
 
     public BufferedImage getImage(){
         return this.image;
     }
+
+    public static BufferedImage setTransparency (BufferedImage image) {
+        BufferedImage transparentImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = transparentImage.createGraphics();
+        g.setComposite(AlphaComposite.SrcOver.derive(0.5f));
+        g.drawImage(image, null, 0, 0);
+        g.dispose();
+        return transparentImage;
+    }
+
+    public int getPollutionIndex(String json) throws ParseException {
+
+        org.json.simple.parser.JSONParser parser = new JSONParser();
+
+        JSONObject jsonObject = (JSONObject) parser.parse(json);
+
+        JSONArray jsonArray = (JSONArray) jsonObject.get("list");
+
+        JSONObject listObject = (JSONObject) jsonArray.getFirst();
+
+        JSONObject mainObject = (JSONObject) listObject.get("main");
+
+        return ((Long) mainObject.get("aqi")).intValue();
+    }
+
+//    public int pollutionIndexMapper(String pollutionIndex) {
+//        Map<String, Integer> indexMap = new HashMap<>();
+//
+//        indexMap.put("GOOD", 1);
+//        indexMap.put("FAIR", 2);
+//        indexMap.put("MODERATE", 3);
+//        indexMap.put("POOR", 3);
+//        indexMap.put("VERY_POOR", 4);
+//
+//        return indexMap.get(pollutionIndex);
+//    }
 
 }
